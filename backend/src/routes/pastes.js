fceb4b8f31ff2d432388047859ed671497c9147c
Sameer-0077 <sameeryadav0077@gmail.com>
2026-02-02 15:42:42 +0530

    Update routes to include new functionality

--- backend/src/routes/pastes.js
+++ backend/src/routes/pastes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { pasteValidation } from "../middlewares/pasteValidation.js";
-import { createPaste } from "../controllers/paste.js";
+import { createPaste, getPaste } from "../controllers/paste.js";
 
 const router = express.Router();
 
 router.post("/pastes", pasteValidation, createPaste);
+router.get("/pastes/:id", getPaste);
 
 export default router;
