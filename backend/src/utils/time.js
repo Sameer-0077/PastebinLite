ea6e55480dc5adcc26f97cc61d959b685b940299
Sameer-0077 <sameeryadav0077@gmail.com>
2026-02-02 19:06:11 +0530

    fix: correct TEST_MODE comparison type in getNow function

--- backend/src/utils/time.js
+++ backend/src/utils/time.js
@@ -1,7 +1,7 @@
 export const getNow = (req) => {
   //   console.log(process.env.TEST_MODE);
 
-  if (process.env.TEST_MODE === "1" && req.headers["x-test-now-ms"]) {
+  if (process.env.TEST_MODE === 1 && req.headers["x-test-now-ms"]) {
     return Number(req.headers["x-test-now-ms"]);
   }
 
