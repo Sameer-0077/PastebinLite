8a5ba282fab3dc81ca2ee1a9d574605f27055e1f
Sameer-0077 <sameeryadav0077@gmail.com>
2026-02-02 15:39:29 +0530

    Refactor paste validation middleware to standardize variable naming and improve error messages

--- backend/src/middlewares/pasteValidation.js
+++ backend/src/middlewares/pasteValidation.js
@@ -1,33 +1,31 @@
 export const pasteValidation = (req, res, next) => {
   try {
-    const { content, ttlSeconds, maxViews } = req.body;
+    const { content, ttl_seconds, max_views } = req.body;
 
     if (!content || typeof content !== "string" || content.trim() === "") {
       return res.status(400).json({ error: "content is requied" });
     }
 
     if (
-      ttlSeconds !== undefined &&
-      (!Number.isInteger(ttlSeconds) || ttlSeconds < 1)
+      ttl_seconds !== undefined &&
+      (!Number.isInteger(ttl_seconds) || ttl_seconds < 1)
     ) {
       return res
         .status(400)
-        .json({ error: "ttlSeconds must be an Ineteger and greater than 0" });
+        .json({ error: "ttl_seconds must be an Ineteger and greater than 0" });
     }
 
     if (
-      maxViews !== undefined &&
-      (!Number.isInteger(maxViews) || maxViews < 1)
+      max_views !== undefined &&
+      (!Number.isInteger(max_views) || max_views < 1)
     ) {
       return res
         .status(400)
-        .json({ error: "maxViews must be an Integer and greater than 0" });
+        .json({ error: "max_views must be an Integer and greater than 0" });
     }
 
     next();
   } catch (error) {
-    return res
-      .status(500)
-      .json({ error: "Internal server error", errorMsg: error.message });
+    return res.status(500).json({ error: "Internal server error" });
   }
 };
